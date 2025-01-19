1c478017adff68b977829300aba5a6227436d576
Harishri2002 <harishribr4@gmail.com>
2025-01-19 13:06:25 +0530

    FeatFeatures />added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Partners from './components/Partner'
+import Features from './components/Features'
 function App() {
   return (
     <>
     <Navbar />
     <Hero />
     <Partners />
+    <Features />
     </>
   )
 }
